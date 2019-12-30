eca070089ad887dbed9db4c08ec020a56a144a12
Sam <samuelriley1393@gmail.com>
2019-12-29 20:21:40 -0500

    added google SSO

---
+++ src/main/java/com/samjones/ThymeleafCrud/security/WebSecurityConfiguration.java
@@ -0,0 +1,25 @@
+//package com.samjones.ThymeleafCrud.security;
+//
+////import org.springframework.cloud.security.oauth2.sso.EnableOAuth2Sso;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+//
+////@EnableOAuth2Sso
+//@Configuration
+//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+////        super.configure(http);
+//        http
+//                .csrf()
+//                .disable()
+//                .antMatcher("/**")
+//                .authorizeRequests()
+//                .antMatchers("/", "", "/index")
+//                    .permitAll()
+//                .anyRequest()
+//                    .authenticated();
+//    }
+//}
